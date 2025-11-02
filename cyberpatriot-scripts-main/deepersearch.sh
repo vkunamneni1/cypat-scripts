9b1c16151dfe619648740cfdce5c9724cad729ea
Veda Kunamneni <154030589+vkunamneni1@users.noreply.github.com>
2025-11-02 10:41:31 -0500

    Update deepersearch.sh

--- cyberpatriot-scripts-main/deepersearch.sh
+++ cyberpatriot-scripts-main/deepersearch.sh
@@ -13,21 +13,21 @@ NC='\033[0m' # No Color
 echo -e "${CYAN}This script was brought to you by atomtables and swaroop for CPC under MCA/EAMS...${NC}"
 read -t 5
 
-echo -e "Listing all media files:\n"
+echo -e "Listing all media files from the *entire* system (this may take a moment):\n"
 
 echo -e "${CYAN}Image files:"
-find /home -type f -regextype egrep -regex '.*\.(jpg|jpeg|png|tiff|bmp|gif)$'
+find / -type f -regextype egrep -regex '.*\.(jpg|jpeg|png|tiff|bmp|gif)$' 2>/dev/null
 echo -e "${MAGENTA}Audio files:"
-find /home -type f -regextype egrep -regex '.*\.(mp3|wav|ogg|flac)$'
+find / -type f -regextype egrep -regex '.*\.(mp3|wav|ogg|flac)$' 2>/dev/null
 echo -e "${BLUE}Video files:"
-find /home -type f -regextype egrep -regex '.*\.(mp4|mov|mkv)'
+find / -type f -regextype egrep -regex '.*\.(mp4|mov|mkv)' 2>/dev/null
 echo -e "${YELLOW}Document files:"
-find /home -type f -regextype egrep -regex '.*\.(txt|docx|doc|xlsx|csv|pptx)'
+find / -type f -regextype egrep -regex '.*\.(txt|docx|doc|xlsx|csv|pptx)' 2>/dev/null
 echo -e "${GREEN}Adobe files:"
-find /home -type f -regextype egrep -regex '.*\.(psd|pdf)'
+find / -type f -regextype egrep -regex '.*\.(psd|pdf)' 2>/dev/null
 echo -e "${RED}Extractables files:"
-find /home -type f -regextype egrep -regex '.*\.(zip|rar|7z|tar|tar.gz|tar.xz)'
+find / -type f -regextype egrep -regex '.*\.(zip|rar|7z|tar|tar.gz|tar.xz)' 2>/dev/null
 echo -e "${WHITE}Executable files:"
-find /home -type f -regextype egrep -regex '.*\.(sh|bash|deb|rpm|appimage)'
+find / -type f -regextype egrep -regex '.*\.(sh|bash|deb|rpm|appimage)' 2>/dev/null
 
-
+echo -e "\nSearch complete."
